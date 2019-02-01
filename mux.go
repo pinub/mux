b94cf9fe6cdb0523abe005fbee31c5f40f5860ff
Danilo <dabio@users.noreply.github.com>
2019-02-01 22:07:40 +0100

    Fix documentation

--- mux.go
+++ mux.go
@@ -3,34 +3,34 @@
 //
 // Example:
 //
-// package main
-//
-// import (
-// 	"log"
-// 	"net/http"
-//
-// 	"github.com/pinub/mux"
-// )
-//
-// func index() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		w.Write([]byte(`Welcome!`))
-// 	}
-// }
-//
-// func hello() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		w.Write([]byte(`Welcome!`))
-// 	}
-// }
-//
-// func main() {
-// 	m := mux.New()
-// 	m.Get("/", index())
-// 	m.Get("/hello", hello())
-//
-// 	log.Fatal(http.ListenAndServe(":8080", m))
-// }
+//  package main
+
+//  import (
+//  	"log"
+//  	"net/http"
+
+//  	"github.com/pinub/mux"
+//  )
+
+//  func index() http.HandlerFunc {
+//  	return func(w http.ResponseWriter, r *http.Request) {
+//  		w.Write([]byte(`Welcome!`))
+//  	}
+//  }
+
+//  func hello() http.HandlerFunc {
+//  	return func(w http.ResponseWriter, r *http.Request) {
+//  		w.Write([]byte(`Welcome!`))
+//  	}
+//  }
+
+//  func main() {
+//  	m := mux.New()
+//  	m.Get("/", index())
+//  	m.Get("/hello", hello())
+
+//  	log.Fatal(http.ListenAndServe(":8080", m))
+//  }
 //
 // The Muxer matches incoming requests by the method and path and delegates
 // to that assiciated handler.
