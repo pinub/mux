7e26c0bfa2b08039766d531a62230fb444f6c40c
Danilo <dabio@users.noreply.github.com>
2019-02-01 22:09:19 +0100

    Fix readme

--- mux.go
+++ mux.go
@@ -4,31 +4,31 @@
 // Example:
 //
 //  package main
-
+//
 //  import (
 //  	"log"
 //  	"net/http"
-
+//
 //  	"github.com/pinub/mux"
 //  )
-
+//
 //  func index() http.HandlerFunc {
 //  	return func(w http.ResponseWriter, r *http.Request) {
 //  		w.Write([]byte(`Welcome!`))
 //  	}
 //  }
-
+//
 //  func hello() http.HandlerFunc {
 //  	return func(w http.ResponseWriter, r *http.Request) {
 //  		w.Write([]byte(`Welcome!`))
 //  	}
 //  }
-
+//
 //  func main() {
 //  	m := mux.New()
 //  	m.Get("/", index())
 //  	m.Get("/hello", hello())
-
+//
 //  	log.Fatal(http.ListenAndServe(":8080", m))
 //  }
 //
